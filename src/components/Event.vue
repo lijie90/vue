cf0fe938068df4cf8625783dd722621fb06699d5
lijie <18120441896@163.com>
2019-01-25 16:31:19 +0800

    1月25日

---
+++ src/components/Event.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>Event</h1>
+    <!-- <button @click="greet">事件触发</button> -->
+    <ul>
+      <li v-for="item in indexObj" :key="item.id" @click="greet(item.id,$event)">{{item.message}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "Event",
+  // props: ["indexObj"],
+  data() {
+    return {
+      indexObj: [{ id: "1", message: "Foo" }, { id: "2", message: "Bar" }]
+    };
+  },
+  methods: {
+    greet: function(id, event) {
+      alert(JSON.stringify(id));
+      alert(JSON.stringify(event.target.key));
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+/* li {
+  display: inline-block;
+  margin: 0 10px;
+} */
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import RoutTestFirst from '@/components/RoutTestFirst'
+import Event from '@/components/Event'
+
 
 
 Vue.use(Router)
@@ -16,6 +18,11 @@ export default new Router({
       path: '/first',
       name: 'RoutTestFirst',
       component: RoutTestFirst
+    },
+    {
+      path: '/event',
+      name: 'Event',
+      component: Event
     }
   ]
 })
